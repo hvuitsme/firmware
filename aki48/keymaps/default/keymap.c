425f1869f72b9deb8871eba543574cafe9b93300
hvuitsme <112574635+hvuitsme@users.noreply.github.com>
2023-06-08 19:11:26 +0700

    Update keymap.c

--- aki48/keymaps/default/keymap.c
+++ aki48/keymaps/default/keymap.c
@@ -7,31 +7,39 @@
 
 enum layer_number{
     _BASE = 0,
-    _FN1,
-    _FN2
+    _FN1 = 1,
+    _FN2 = 2,
+    _FN3 = 3
 };
+
 
 #define _______ KC_TRANSPARENT
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
-        QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        KC_LCTL, _______, KC_LALT, MO(_FN2),MO(_FN1),KC_SPC,  KC_MUTE,  KC_SPC,  _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, MO(_FN2),MO(_FN1),KC_SPC,  KC_MUTE,  KC_SPC,  KC_ENT,  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
     ),
     [_FN1] = LAYOUT(
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
+        KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
+        KC_LCTL, XXXXXXX, XXXXXXX, _______, _______, KC_SPC, _______,  KC_SPC,  KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [_FN2] = LAYOUT(
-        QK_BOOT, _______, _______, RGB_SAI, RGB_HUI, RGB_SPI,          _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, RGB_SAD, RGB_HUD, RGB_SPD,          _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, RGB_VAI,          _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, RGB_VAD, RGB_TOG, _______, _______, _______, _______, _______, _______
+        KC_ESC,  KC_EXLM, KC_AT,   KC_HASH,  KC_DLR, KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+        KC_TAB,  KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
+        KC_LSFT, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_RSFT,
+        KC_LCTL, XXXXXXX, XXXXXXX, _______, _______, KC_SPC, _______,  KC_SPC,  KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+    [_FN3] = LAYOUT(
+        QK_BOOT, XXXXXXX, XXXXXXX, RGB_SAI, RGB_HUI, RGB_SPI,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, RGB_SAD, RGB_HUD, RGB_SPD,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_VAD, RGB_VAI,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, KC_SPC,  RGB_TOG, KC_SPC,  KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
 
@@ -114,7 +122,8 @@ bool oled_task_user(void) {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
     [_FN1] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
-    [_FN2] =  { ENCODER_CCW_CW(RGB_MOD, RGB_RMOD) },
+    [_FN2] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [_FN3] =  { ENCODER_CCW_CW(RGB_MOD, RGB_RMOD) },
 };
 #endif
 
@@ -126,7 +135,7 @@ bool encoder_update_user(uint8_t index, bool clockwise){
         } else {
             tap_code(KC_VOLD);
         }
-    } else if (IS_LAYER_ON(_FN2)) {
+    } else if (IS_LAYER_ON(_FN3)) {
         if (clockwise){
             rgblight_step();
         } else {
