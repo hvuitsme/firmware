d07463e42c97cab4d01d314f33c4835900463a58
hvuitsme <112574635+hvuitsme@users.noreply.github.com>
2023-06-20 22:50:37 +0700

    Add files via upload

--- aki48/keymaps/default/keymap.c
+++ aki48/keymaps/default/keymap.c
@@ -14,25 +14,76 @@ enum layer_number{
 #define _______ KC_TRANSPARENT
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
+    /* BASE
+     * ┌───┬───┬───┬───┬───┬───┐      ┌───┬───┬───┬───┬───┬───┐
+     * │ESC│ Q │ W │ E │ R │ T │      │ Y │ U │ I │ O │ P │BSC│
+     * ├───┼───┼───┼───┼───┼───┤      ├───┼───┼───┼───┼───┼───┤
+     * │TAB│ A │ S │ D │ F │ G │      │ H │ J │ K │ L │ ; │ ' │
+     * ├───┼───┼───┼───┼───┼───┤      ├───┼───┼───┼───┼───┼───┤
+     * │LSF│ Z │ X │ C │ V │ B │      │ N │ M │ , │ . │ / │RSF│
+     * ├───┼───┼───┼───┼───┼───┼──────┼───┼───┼───┼───┼───┼───┤
+     * │LCT│LWN│ALT│FN1│FN2│SPC│ MUTE │SPC│ENT│ ← │ ↑ │ ↓ │ → │
+     * └───┴───┴───┴───┴───┼───┴──────┴───┼───┴───┴───┴───┴───┘
+     *                     │     space    │
+     *                     └──────────────┘
+     */ 
     [_BASE] = LAYOUT(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT, MO(_FN2),MO(_FN1),KC_SPC,  KC_SPC,   KC_SPC,  KC_ENT,  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
     ),
+    /* FN1
+     * ┌───┬───┬───┬───┬───┬───┐      ┌───┬───┬───┬───┬───┬───┐
+     * │ESC│ 1 │ 2 │ 3 │ 4 │ 5 │      │ 6 │ 7 │ 8 │ 9 │ 0 │BSC│
+     * ├───┼───┼───┼───┼───┼───┤      ├───┼───┼───┼───┼───┼───┤
+     * │TAB│XXX│XXX│XXX│XXX│XXX│      │XXX│XXX│XXX│XXX│XXX│DEL│
+     * ├───┼───┼───┼───┼───┼───┤      ├───┼───┼───┼───┼───┼───┤
+     * │LSF│XXX│XXX│XXX│XXX│XXX│      │XXX│XXX│XXX│XXX│XXX│RSF│
+     * ├───┼───┼───┼───┼───┼───┼──────┼───┼───┼───┼───┼───┼───┤
+     * │LCT│XXX│ALT│ ↓ │FN2│SPC│      │SPC│ENT│XXX│XXX│XXX│XXX│
+     * └───┴───┴───┴───┴───┼───┴──────┴───┼───┴───┴───┴───┴───┘
+     *                     │     space    │
+     *                     └──────────────┘
+     */
     [_FN1] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
         KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
         KC_LCTL, XXXXXXX, XXXXXXX, _______, _______, KC_SPC,  KC_SPC,  KC_SPC,  KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
+    /* FN2
+     * ┌───┬───┬───┬───┬───┬───┐      ┌───┬───┬───┬───┬───┬───┐
+     * │ESC│ ! │ @ │ # │ $ │ % │      │ ^ │ & │ * │ ( │ ) │BSC│
+     * ├───┼───┼───┼───┼───┼───┤      ├───┼───┼───┼───┼───┼───┤
+     * │TAB│ `~│XXX│XXX│XXX│XXX│      │-_ │=+ │[{ │]} │\| │DEL│
+     * ├───┼───┼───┼───┼───┼───┤      ├───┼───┼───┼───┼───┼───┤
+     * │LSF│ ~ │XXX│XXX│XXX│XXX│      │XXX│XXX│XXX│XXX│XXX│RSF│
+     * ├───┼───┼───┼───┼───┼───┼──────┼───┼───┼───┼───┼───┼───┤
+     * │LCT│XXX│ALT│FN1│ ↓ │SPC│      │SPC│ENT│XXX│XXX│XXX│XXX│
+     * └───┴───┴───┴───┴───┼───┴──────┴───┼───┴───┴───┴───┴───┘
+     *                     │     space    │
+     *                     └──────────────┘
+     */
     [_FN2] = LAYOUT(
         KC_ESC,  KC_EXLM, KC_AT,   KC_HASH,  KC_DLR, KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
         KC_TAB,  KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
-        KC_LSFT, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_RSFT,
+        KC_LSFT, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
         KC_LCTL, XXXXXXX, XXXXXXX, _______, _______, KC_SPC,  KC_SPC,  KC_SPC,  KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
+    /* FN3 (FN1+FN2)
+     * ┌───┬───┬───┬───┬───┬───┐      ┌───┬───┬───┬───┬───┬───┐
+     * │ESC│XXX│XXX│SA+│HUI│SP+│      │XXX│XXX│XXX│XXX│XXX│XXX│
+     * ├───┼───┼───┼───┼───┼───┤      ├───┼───┼───┼───┼───┼───┤
+     * │XXX│XXX│XXX│SA-│HUD│SP-│      │XXX│XXX│XXX│XXX│XXX│XXX│
+     * ├───┼───┼───┼───┼───┼───┤      ├───┼───┼───┼───┼───┼───┤
+     * │XXX│XXX│XXX│XXX│VAD│VAI│      │XXX│XXX│XXX│XXX│XXX│XXX│
+     * ├───┼───┼───┼───┼───┼───┼──────┼───┼───┼───┼───┼───┼───┤
+     * │XXX│XXX│XXX│ ↓ │ ↓ │XXX│      │XXX│XXX│XXX│XXX│XXX│XXX│
+     * └───┴───┴───┴───┴───┼───┴──────┴───┼───┴───┴───┴───┴───┘
+     *                     │      XXX     │
+     *                     └──────────────┘
+     */
     [_FN3] = LAYOUT(
         QK_BOOT, XXXXXXX, XXXXXXX, RGB_SAI, RGB_HUI, RGB_SPI,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, RGB_SAD, RGB_HUD, RGB_SPD,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
